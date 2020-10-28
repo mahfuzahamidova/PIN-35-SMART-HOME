0ff534e110258476decbad427aadf82bbf990c57
Mahfuza Hamidova <makhfuza.hamidova@gmail.com>
2020-10-28 10:02:17 +0300

    added greeting Git info

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+
 using namespace std;
+
 int main() {
     char name[20];
     cout << "Enter your name: ";
     cin >> name;
     cout << "Hello, " << name << "!\n";
+    cout << "Nice to see you learning Git\n";
     system("pause");
     return 0;
 }
